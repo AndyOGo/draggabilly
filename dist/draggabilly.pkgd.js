050398f59cc0f4ca2d10f12ce6214ec28cebf37b
Andreas Deuschlinger <andreas.deuschlinger@easysys.com>
2015-05-11 11:10:28 +0200

    gulp dist

--- dist/draggabilly.pkgd.js
+++ dist/draggabilly.pkgd.js
@@ -2019,16 +2019,29 @@ Draggabilly.prototype._addTransformPosition = function( style ) {
  * @param {Event or Touch} pointer
  */
 Draggabilly.prototype.setDropTarget = cssPointerEvents ?
-  function(event) {
-    event.dropTarget = event.target;
-  } :
-  function(event, pointer) {
-    var style = this.element.style;
-    style.display = 'none';
-    event.dropTarget = document.elementFromPoint(pointer.clientX, pointer.clientY);
-    style.display = '';
-  };
+  setDropTargetByCSS : setDropTargetByJS;
 
+function setDropTargetByCSS(event, pointer) {
+  var that = this,
+    target = event.target,
+    handle = that.options.handle;
+
+  if(handle && target.className.indexOf(handle.substr(1)) ||
+  target === that.element) {
+    Draggabilly.prototype.setDropTarget = setDropTargetByJS;
+    that.setDropTarget(event, pointer);
+    return;
+  }
+
+  event.dropTarget = event.target;
+}
+
+function setDropTargetByJS(event, pointer) {
+  var style = this.element.style;
+  style.display = 'none';
+  event.dropTarget = document.elementFromPoint(pointer.clientX, pointer.clientY);
+  style.display = '';
+}
 // -------------------------- events -------------------------- //
 
 /**
